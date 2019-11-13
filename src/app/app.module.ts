0abfa899509499e14e04cf3a0e3c9df7abaf7095
Khaled Amriki <khamriki@saudia.com>
2019-11-13 22:18:38 +0300

    1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { MediaCapture } from '@ionic-native/media-capture/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    MediaCapture, SocialSharing
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MediaCapture, MediaFile } from '@ionic-native/media-capture/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public mediaCapture: MediaCapture,
+    public socialSharing: SocialSharing
+  ) {}
 
+  CaptureAudio() {
+    this.mediaCapture.captureAudio().then((audio:MediaFile[]) => {
+      this.ShareMedia('audio captures by X-APP', 'Audio Subject', audio[0].fullPath, '');
+    }, (err) => {alert(JSON.stringify(err))});
+  }
+
+  CaptureVideo() {
+    this.mediaCapture.captureVideo().then((video:MediaFile[]) => {
+      this.ShareMedia('video captures by X-APP', 'Video Subject', video[0].fullPath, '');
+    }, (err) => {alert(JSON.stringify(err))});
+  }
+
+  CaptureImage() {
+    this.mediaCapture.captureImage().then((image:MediaFile[]) => {
+      this.ShareMedia('image captures by X-APP', 'Image Subject', image[0].fullPath, '');
+    }, (err) => {alert(JSON.stringify(err))});
+  }
+
+  ShareMedia (message, subject, filepath, url) {
+    this.socialSharing.share(message, subject, filepath, url).then(() => {
+
+    }, (err) => {alert(JSON.stringify(err))});
+  }
 }
